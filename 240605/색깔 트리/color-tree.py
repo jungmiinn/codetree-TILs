84cd822038e3cad2f174685d6edb30c30455ccbd
Jungmin Park <76022058+jungmiinn@users.noreply.github.com>
2024-06-05 22:56:53 +0900

    Title: 색깔 트리, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240605/색깔 트리/color-tree.py
@@ -0,0 +1,81 @@
+n = int(input()) # 명령횟수
+node = []
+
+
+def node_add(mid, pid, color, max_d): #100
+    # print("노드추가")
+    new_node = [mid, pid, color, max_d]
+    if pid != -1:
+        depth = 1
+        for x in node:
+            if x[0] == pid:
+                depth += 1
+                if x[3] < depth:
+                    # print("깊이제한")
+                    return 0
+
+    node.append(new_node)
+    # return 0
+
+
+def change_color(mid, color): #200
+    # print("색깔변경")
+    id = mid
+    for x in node:
+        if x[0] == mid: # 루트노드 찾기
+            x[2] = color
+        if x[1] == id: # 자식노드 찾기
+            id = x[0]
+            x[2] = color
+    # return 0
+
+
+def find_color(mid): #300
+    # print("색깔조회")
+    for x in node:
+        if x[0] == mid:
+            return x[2]
+
+
+def cal_score(): #400
+    # print("점수확인")
+    score = []
+    def dfs(start, color):
+        # print("start:", start)
+        root = start[0]
+        node_track = []
+        for x in node:
+            # print("root, color 현황:", root, color)
+            if x[1] ==  root:
+                node_track.append(x)
+                color.append(x[2])
+        
+        if len(node_track) > 0:
+            new = node_track.pop()
+            dfs(new, color)
+        else:
+            # print("color:", color)
+            point = len(set(color))
+            score.append(point**2)
+    
+    for i in range(len(node)): 
+        # print(score)
+        dfs(node[i], [node[i][2]])      
+    return sum(score)
+
+
+
+for i in range(n):
+    # print("노드 현황:", node)
+    temp = list(map(int, input().split(" ")))
+    # print(temp)
+    if temp[0] == 100:
+        node_add(temp[1], temp[2], temp[3], temp[4])
+    elif temp[0] == 200:
+        change_color(temp[1], temp[2])
+    elif temp[0] == 300:
+        print(find_color(temp[1]))
+    elif temp[0] == 400:
+        # print("node 현황:", node)
+        print(cal_score())
+    temp = ""
