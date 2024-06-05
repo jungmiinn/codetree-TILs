61cf25bffc2caa659a969f470b5f8279165f25d5
Jungmin Park <76022058+jungmiinn@users.noreply.github.com>
2024-06-06 02:17:59 +0900

    Title: 마법의 숲 탐색, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240606/마법의 숲 탐색/magical-forest-exploration.py
+++ 240606/마법의 숲 탐색/magical-forest-exploration.py
@@ -11,8 +11,11 @@ def moving_inside(i, res):
         a, b = x[0], x[1]
         if a >= 0 and a < ip[0] and b >= 0 and b < ip[1]:
             if forest[a][b] > 0  and forest[a][b] != i:
-                res.append(forest[a][b])
-                moving_inside(forest[a][b], res)
+                if forest[a][b] in res:
+                    return res
+                else:
+                    res.append(forest[a][b])
+                    moving_inside(forest[a][b], res)
     return res
 
 
@@ -23,18 +26,19 @@ def putdown(s1, s2, ex, i):
     flag = True
     now1 = s1
     now2 = s2
-
+    while True:
+        if now1 + 2 == ip[0]:
+            break
+        else:
+            if forest[now1+2][now2] == 0:
+                now1 += 1
+            else:
+                break
     while flag:
         # 아래 방향으로 이동할 때 배열 범위 체크
-        if now1 + 1 >= ip[0]-1:
+        if now1 + 2 == ip[0]:
             flag = False
             break
-
-        if (now1 + 2 < ip[0] and
-            forest[now1 + 2][now2] == 0 and 
-            forest[now1 + 1][now2 - 1] == 0 and 
-            forest[now1 + 1][now2 + 1] == 0):  # 아래방향
-            now1 += 1
         else:
             # 왼쪽 방향 (반시계 90 회전)
             if (now2 - 2 >= 0  and 
@@ -48,6 +52,7 @@ def putdown(s1, s2, ex, i):
                     forest[now1 + 1][now2 - 1] == 0):
                     now1 += 1
                     now2 -= 1
+                    print("좌로회전")
                     ex = 3 if ex == 0 else ex - 1
                 else:
                     flag = False
@@ -65,13 +70,16 @@ def putdown(s1, s2, ex, i):
                     now1 += 1
                     now2 += 1
                     ex = 0 if ex == 3 else ex + 1
+                    print("우로회전")
                 else:
+                    print("여기니?")
                     flag = False
                     break
             else:
                 if now1 - 1 < 0:
+                    print("진입불가 리셋")
                     return 0
-
+                print("양쪽으로도 회전 불가")
                 flag = False
                 break
 
@@ -86,8 +94,8 @@ def putdown(s1, s2, ex, i):
 
     exit_info[i] = [door[ex][0],door[ex][1]] # 출구 메모
     parking_info[i] = [[now1, now2], [now1-1, now2], [now1, now2+1], [now1+1, now2], [now1, now2-1]]
-    ret = moving_inside(i, [])  # 갈수있는 골렘 번호
-
+    ret = set(moving_inside(i, []))  # 갈수있는 골렘 번호
+    # print(parking_info)
     if len(ret) > 0:
         mx = 0
         for i in ret:
@@ -100,10 +108,10 @@ def putdown(s1, s2, ex, i):
 
 score = 0
 for i in range(ip[2]):
-    # print(i+1, "번째")
+    print(i+1, "번째")
     comm = list(map(int, input().split(" ")))
     res = putdown(-2, comm[0] - 1, comm[1], i+1)
-    # print("res:", res)
+    print("res:", res)
     if res == 0: #reset 
         forest = [[0 for i in range(ip[1])] for j in range(ip[0])]
         parking_info = {}
