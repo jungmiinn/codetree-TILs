7126ac831b1dd6bfd80b4431fbfdb72fc147a45b
Jungmin Park <76022058+jungmiinn@users.noreply.github.com>
2024-07-09 20:38:05 +0900

    Title: 연속되는 수, Time: 106ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240709/연속되는 수/continuous-number.py
@@ -0,0 +1,26 @@
+N = int(input())
+A = [int(input()) for _ in range(N)]
+Aset = set(A)
+M = 0
+
+def checking(arr):
+    cnt = 1
+    max_cnt = 1
+    for i in range(1, len(arr)):
+        if arr[i-1] == arr[i]:
+            cnt += 1
+        else:
+            max_cnt = max(max_cnt, cnt)
+            cnt = 1
+    return max_cnt
+
+
+for n in Aset:
+    Acopy = []
+    for a in A:
+        if a != n:
+            Acopy.append(a)
+    cnt = checking(Acopy)
+    M = max(M, cnt)
+
+print(M)
